c21b0cc353a987d378a0f2ecbe09ef699102b6c2
hangingnoodle <nils.hoefer1997@gmail.com>
2021-04-24 14:44:02 +0200

    Update

--- Team_1/Interface.java
+++ Team_1/Interface.java
@@ -1,7 +1,7 @@
 package Team_1;
 
 public interface Interface {
-    public abstract boolean isWellSorted(String[] sequence);
-    public abstract boolean isWellSorted(String[][] sequence);
+    public boolean isWellSorted(String[] sequence);
+    public boolean isWellSorted(String[][] sequence);
 
 }
--- Team_1/Programmierer1.java
+++ Team_1/Programmierer1.java
@@ -1,14 +1,14 @@
 package Team_1;
 public class Programmierer1 implements Interface {
-
     public boolean isWellSorted( String[] sequence ) {
         for (int i = 0; i < sequence.length - 1; i++) {
-            String s1 = sequence[i];
-            String s2 = sequence[i + 1];
+            String s1 = sequence[i].toLowerCase();
+            String s2 = sequence[i + 1].toLowerCase();
 
             if (s1.compareTo(s2) > 0)
                 return false;
         }
+
         return true;
     }
 
@@ -20,9 +20,5 @@ public class Programmierer1 implements Interface {
         }
 
         return true;
-    }
-
-    public static void main(String[] args) {
-        
     }
 }
