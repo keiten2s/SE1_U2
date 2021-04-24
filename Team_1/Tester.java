636c4c2dc4c4cf4f603b71cdb8009e15b6f0be7e
Boostinettenbear <122537c5054122@gmail.com>
2021-04-24 13:47:57 +0200

    tester1.java

---
+++ Team_1/Tester.java
@@ -0,0 +1,57 @@
+package Team_1;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+
+public class Tester {
+
+    @BeforeEach
+    public void setUP(){
+
+        String[][] a = new Programmierer1();
+    }
+
+
+
+    @Test
+    @DisplayName("Array ist  vorhanden")
+    void isnull(){
+        assertNull(a);
+
+    }
+
+    @Test
+    @DisplayName("Array ist leer")
+    void isempty(){
+        assertTrue();
+
+    }
+
+
+    @Test
+    @DisplayName("Array sind Buchstaben")
+    void isletters(){
+        assertTrue();
+
+    }
+
+  
+    @Test
+    @DisplayName("Array ist sortiert")
+    void issorted(){
+        assertTrue(a.iswellSorted( ));
+
+    }
+
+    @Test
+    @DisplayName("Array mit gleichem Inhalt")
+    void siequals(){
+        assertEquals();
+
+    }
+
+}
